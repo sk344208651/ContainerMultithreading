b611077ebbef559b9c57da8a4ce8e559e745573c
SkyShen <344208651@qq.com>
2017-04-06 19:50:56 +0800

    hh

--- ContainerMultithreading/ViewController.swift
+++ ContainerMultithreading/ViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
+enum ThreadRunningWay {
+    case none
+    case safe
+    case unsafe
+}
+
 class ViewController: UIViewController {
+    
+    
+    
+    var threadSafewayContinuing = false
+    var threadUnSafewayContinuing = false
+    
+    var runningWay : ThreadRunningWay = .none
+    var operation : BlockOperation?
+    
+    var dictionary : [String : String]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        operation = BlockOperation()
+//        dictionary = dic
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func threadSafeWay(_ sender: UIButton) {
+        operation!.addExecutionBlock(safeCopyingDictionaryClosure)
+    }
 
+    @IBAction func nonThreadSafeWay(_ sender: UIButton) {
+        operation!.addExecutionBlock(unsafeCopyingDictionaryClosure)
+    }
+    
+    
+    // MARK: - closure to operate dictionary
+    
+    var safeCopyingDictionaryClosure = { () -> Void in
+        while(runningWay != .none){
+            
+        }
+    }
+    
+    var unsafeCopyingDictionaryClosure = { () -> Void in
+        
+    }
 }
+
+
+
+
+
+
+
 
